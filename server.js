439f277fccb0dc17c31a8634ff06957d27ac26a6
ceaselesslogout <ceaselesslogout@gmail.com>
2025-12-02 12:42:29 +0000

    fixed server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "../uploads")));
 
 // Connect MongoDB (we will fill this later)
-mongoose.connect("mongodb+srv://ceaselesslogout:Makeins1@cluster0.rzuy9sl.mongodb.net/?&appName=cluster0")
+mongoose.connect("process.env.MONGO_URL")
     .then(() => console.log("MongoDB Connected"))
     .catch(err => console.log(err));
 
@@ -101,7 +101,7 @@ app.get("/", (req, res) => {
     res.send("Backend is running online!");
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log("Server running on port " + PORT);
 }); 
